386123cd189e40238f837a52c313ac52b6336035
Deepak KC <deepak.curator@gmail.com>
2022-02-14 08:15:44 +0200

    task comments

--- lect2/groupTask_fileh.php
+++ lect2/groupTask_fileh.php
@@ -1,7 +1,9 @@
 <? 
 //You are required to work as a team and complete the following task during the online session
-// we will check your implementation on 16.02.2022
-
+// we will check your implementation on 16.02.2022 and the team repo must contain the following task
+// Each one of you will first do it in your own repo (or branch) and then the final version in the team repo. 
+// This task is not graded however will have some impact on the final grade
+// It also will help you to practice utlizing GitHub in project work
 # 1. Create/read a text file by using approprite php functions 
     # Step 1: check if file exists or not
     # Step 2: Open the file using appropriate mode. (each member opens the file in different mode)
